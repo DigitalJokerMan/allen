4f2a284fa974d5b3080d7f72de483ca1d73bf6b2
Alex K <aiden.hapless@gmail.com>
2022-12-11 16:40:16 -0700

    Lock context for listener properties

--- src/listener.rs
+++ src/listener.rs
@@ -7,6 +7,8 @@ pub struct Listener {
 
 impl PropertiesContainer<f32> for Listener {
     fn get(&self, param: i32) -> f32 {
+        let _lock = self.context.make_current();
+
         let result = unsafe {
             let mut value = 0.0;
             alGetListenerf(param, &mut value);
@@ -19,6 +21,8 @@ impl PropertiesContainer<f32> for Listener {
     }
 
     fn set(&self, param: i32, value: f32) {
+        let _lock = self.context.make_current();
+
         unsafe { alListenerf(param, value) };
         check_al_error().unwrap();
     }
@@ -26,6 +30,8 @@ impl PropertiesContainer<f32> for Listener {
 
 impl PropertiesContainer<[f32; 3]> for Listener {
     fn get(&self, param: i32) -> [f32; 3] {
+        let _lock = self.context.make_current();
+
         let result = unsafe {
             let mut value = [0.0, 0.0, 0.0];
             alGetListener3f(param, &mut value[0], &mut value[1], &mut value[2]);
@@ -38,6 +44,8 @@ impl PropertiesContainer<[f32; 3]> for Listener {
     }
 
     fn set(&self, param: i32, value: [f32; 3]) {
+        let _lock = self.context.make_current();
+
         unsafe { alListener3f(param, value[0], value[1], value[2]) };
         check_al_error().unwrap()
     }
@@ -45,12 +53,16 @@ impl PropertiesContainer<[f32; 3]> for Listener {
 
 impl PropertiesContainer<Orientation> for Listener {
     fn get(&self, param: i32) -> Orientation {
+        let _lock = self.context.make_current();
+
         let mut value = Orientation::default();
         unsafe { alGetListenerfv(param, &mut value as *mut Orientation as *mut f32) };
         value
     }
 
     fn set(&self, param: i32, value: Orientation) {
+        let _lock = self.context.make_current();
+
         unsafe { alListenerfv(param, &value as *const Orientation as *const f32) };
     }
 }
